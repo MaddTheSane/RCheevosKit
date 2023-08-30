8ab7b011fc1e0abacdfee5387d14a27c2bb52b2d
C.W. Betts <computers57@hotmail.com>
2023-08-29 20:44:15 -0600

    Initial Commit

---
+++ RcheevosKit/RcheevosKit.h
@@ -0,0 +1,18 @@
+//
+//  RcheevosKit.h
+//  RcheevosKit
+//
+//  Created by C.W. Betts on 8/29/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RcheevosKit.
+FOUNDATION_EXPORT double RcheevosKitVersionNumber;
+
+//! Project version string for RcheevosKit.
+FOUNDATION_EXPORT const unsigned char RcheevosKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RcheevosKit/PublicHeader.h>
+
+
